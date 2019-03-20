a5615b50a8c2bd07a72c8846f6e576df00f40ad4
dr-benway <dr-benway@users.noreply.github.com>
2019-03-20 12:52:50 +0200

    fix --continue_train & model mode type

--- options/base_options.py
+++ options/base_options.py
@@ -5,15 +5,13 @@ import torch
 import models
 import data
 
-
 class BaseOptions():
     def __init__(self):
         self.initialized = False
-
     def initialize(self, parser):
         parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
-        parser.add_argument('--loadSize', type=int, default=286, help='scale images to this size')
+        parser.add_argument('--loadSize', type=int, default=496, help='scale images to this size')
         parser.add_argument('--fineSize', type=int, default=256, help='then crop to this size')
         parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels')
         parser.add_argument('--n_downsampling', type=int, default=2, help='# downsampling layers')
@@ -26,8 +24,8 @@ class BaseOptions():
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded. [unaligned | aligned | single]')
-        parser.add_argument('--model', type=str, default='cycle_gan',
-                            help='chooses which model to use. cycle_gan, pix2pix, test')
+        parser.add_argument('--model', type=str, default='unpaired_revgan',
+                            help='chooses which model to use. unpaired_revgan, unpaired_revgan3d')
         parser.add_argument('--which_direction', type=str, default='AtoB', help='AtoB or BtoA')
         parser.add_argument('--nThreads', default=4, type=int, help='# threads for loading data')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
@@ -43,6 +41,7 @@ class BaseOptions():
         parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
         parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal|xavier|kaiming|orthogonal]')
         parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
+        parser.add_argument('--epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
         parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{which_model_netG}_size{loadSize}')
         parser.add_argument('--no_output_tanh', action='store_true', help='end models with tanh activation function')
@@ -53,28 +52,20 @@ class BaseOptions():
         return parser
 
     def gather_options(self):
-        # initialize parser with basic options
         if not self.initialized:
             parser = argparse.ArgumentParser(
                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
             parser = self.initialize(parser)
-
-        # get the basic options
         opt, _ = parser.parse_known_args()
 
-        # modify model-related parser options
         model_name = opt.model
         model_option_setter = models.get_option_setter(model_name)
         parser = model_option_setter(parser, self.isTrain)
-        opt, _ = parser.parse_known_args()  # parse again with the new defaults
-
-        # modify dataset-related parser options
+        opt, _ = parser.parse_known_args()
         dataset_name = opt.dataset_mode
         dataset_option_setter = data.get_option_setter(dataset_name)
         parser = dataset_option_setter(parser, self.isTrain)
-
         self.parser = parser
-
         return parser.parse_args()
 
     def print_options(self, opt):
@@ -91,7 +82,6 @@ class BaseOptions():
         if not opt.mute_optprint:
             print(message)
 
-        # save to the disk
         expr_dir = os.path.join(opt.checkpoints_dir, opt.name)
         util.mkdirs(expr_dir)
         file_name = os.path.join(expr_dir, 'opt.txt')
@@ -100,18 +90,14 @@ class BaseOptions():
             opt_file.write('\n')
 
     def parse(self):
-
         opt = self.gather_options()
-        opt.isTrain = self.isTrain   # train or test
+        opt.isTrain = self.isTrain
 
-        # process opt.suffix
         if opt.suffix:
             suffix = ('_' + opt.suffix.format(**vars(opt))) if opt.suffix != '' else ''
             opt.name = opt.name + suffix
-
         self.print_options(opt)
 
-        # set gpu ids
         str_ids = opt.gpu_ids.split(',')
         opt.gpu_ids = []
         for str_id in str_ids:
@@ -120,6 +106,6 @@ class BaseOptions():
                 opt.gpu_ids.append(id)
         if len(opt.gpu_ids) > 0:
             torch.cuda.set_device(opt.gpu_ids[0])
-
         self.opt = opt
         return self.opt
+    
